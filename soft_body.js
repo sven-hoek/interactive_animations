7ac2980add84368a838846771d16c9bfa0f330c9
sven-hoek <herrlauch@gmail.com>
2025-04-04 21:19:00 +0800

    Move potential keyboard stuff into input.js

--- soft_body.js
+++ soft_body.js
@@ -1,36 +1,3 @@
-
-/*
-var rightPressed = false;
-var leftPressed = false;
-var upPressed = false;
-document.addEventListener("keydown", keyDownHandler, false);
-document.addEventListener("keyup", keyUpHandler, false);
-document.addEventListener("mousemove", mouseMoveHandler, false);
-function keyDownHandler(e) {
-    if(e.key == "Right" || e.key == "ArrowRight" || e.key == "d") {
-        rightPressed = true;
-    }
-    else if(e.key == "Left" || e.key == "ArrowLeft" || e.key == "a") {
-        leftPressed = true;
-    }
-    else if(e.key == "Up" || e.key == "ArrowUp" || e.key == "w") {
-        upPressed = true;
-    }
-}
-function keyUpHandler(e) {
-    if(e.key == "Right" || e.key == "ArrowRight" || e.key == "d") {
-        rightPressed = false;
-    }
-    else if(e.key == "Left" || e.key == "ArrowLeft" || e.key == "a") {
-        leftPressed = false;
-    }
-    else if(e.key == "Up" || e.key == "ArrowUp" || e.key == "w") {
-        upPressed = false;
-    }
-}
-*/
-
-
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Class representing a point of an elastic blob
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
--- src/input.js
+++ src/input.js
@@ -8,3 +8,34 @@ document.addEventListener("mousemove", (e) => {
 });
 document.addEventListener("mousedown", (e) => { mouse_state.isDown = true; });
 document.addEventListener("mouseup", (e) => { mouse_state.isDown = false; });
+
+/*
+var rightPressed = false;
+var leftPressed = false;
+var upPressed = false;
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+document.addEventListener("mousemove", mouseMoveHandler, false);
+function keyDownHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight" || e.key == "d") {
+        rightPressed = true;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft" || e.key == "a") {
+        leftPressed = true;
+    }
+    else if(e.key == "Up" || e.key == "ArrowUp" || e.key == "w") {
+        upPressed = true;
+    }
+}
+function keyUpHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight" || e.key == "d") {
+        rightPressed = false;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft" || e.key == "a") {
+        leftPressed = false;
+    }
+    else if(e.key == "Up" || e.key == "ArrowUp" || e.key == "w") {
+        upPressed = false;
+    }
+}
+*/
