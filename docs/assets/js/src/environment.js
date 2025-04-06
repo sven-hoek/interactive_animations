7a6977aabd248357f4c2a4a7dc95e114190b1704
sven-hoek <herrlauch@gmail.com>
2025-04-06 19:15:55 +0800

    Disable scrolling with touch on canvas

--- docs/assets/js/src/environment.js
+++ docs/assets/js/src/environment.js
@@ -3,7 +3,7 @@ class Environment2D {
         this.canvas = document.getElementById(canvas_element_id);
         console.log("body width:", this.canvas.parentElement.offsetWidth);
         this.canvas.width = this.canvas.parentElement.offsetWidth;
-        this.canvas.height = window.innerHeight * 0.9;
+        this.canvas.height = window.innerHeight * 0.8;
         this.canvas.center = new Vector(this.canvas.width / 2, this.canvas.height / 2)
         this.canvas.ul = new Vector(0, 0);
         this.canvas.br = new Vector(this.canvas.width, this.canvas.height);
